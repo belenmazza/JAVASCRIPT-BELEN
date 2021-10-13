f6f72a2aa845e3582ff946d32b6da0c59d085a7b
belenmazza <belenmazza200@gmail.com>
2021-10-13 12:08:47 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Mensaje de prueba")
+let nombre = prompt("Ingresá tu nombre");
+let apellido = prompt("Ingresá tu apellido");
+let genero = prompt("Ingresá tu genero");
+
+alert(`Hola ${nombre} ${apellido}, sos ${genero} !`)
