b255272eb6bc9459bb97d6fea4a4172f613763e8
belenmazza <belenmazza200@gmail.com>
2021-10-24 20:36:28 -0300

    Desafio 3

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-let edad = parseInt(prompt ("Ingrese su edad")) 
+let numero1 = parseFloat (prompt("Ingrese numero"))
 
-if (edad >= 18){
-    console.log ("Mayor de edad")
-} else{
-    console.log ("Menor de edad")
+for(let i = 1; i<= 10; i++) {
+    console.log(`Numero ${numero1} sumado mas ${i} es igual a ${numero1 + i}`)
 }
-
