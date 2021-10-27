80955b786f860de4f9a2edf1a1065bbf09bfc635
belenmazza <belenmazza200@gmail.com>
2021-10-26 21:17:18 -0300

    Desafio 4

--- script.js
+++ script.js
@@ -1,45 +1,10 @@
-let opcion = prompt("Ingrese si desea Rogel o Brownie")
-let cantidad = parseInt(prompt("Ingrese cuantos quiere"));
+/*CALCULAR VALOR DE CADA CUOTA*/
 
-let rogelDisponible = 1;
-let brownieDisponible = 1;
-
-
-if (opcion.toUpperCase() === "ROGEL") {
-    verificarStock(cantidad, rogelDisponible)
-} else if(opcion.toUpperCase() === "BROWNIE") {
-    verificarStock(cantidad, brownieDisponible)
-} else {
-    alert("El numero ingresado es invalido")
+function suma (numero1, numero2) {
+    return (numero1 / numero2)
 }
 
-function verificarStock (postreIngresado, stockDisponible) {
-    if(postreIngresado === stockDisponible){
-        solicitarDatos()
-    } else {
-        alert("Se agotó el stock del postre. Puede elegir otro o encargarlo")
-    }
-}
+let numero1 = parseFloat (prompt ("Ingrese precio del producto"))
+let numero2 = parseFloat (prompt (" Ingrese cantidad de cuotas: 3/6/12"))
 
-function solicitarDatos (){
-    let nombre = prompt("Ingrese su nombre")
-    let entrega = prompt("Ingrese modalidad de entrega")
-    let tamaño = prompt ("Ingrese el tamaño del postre: grande o chico")
-
-    while(nombre === "" || nombre === null || tamaño === "" || tamaño === null|| entrega === "" || entrega === null) {
-        alert("Los valores ingresados son incorrectos, intente nuevamente")
-        nombre = prompt("Ingrese su nombre")
-        tamaño = prompt("Ingrese el tamaño del postre: grande o chico")
-        entrega = prompt("Ingrese modalidad de entrega")
-
-    }
-    
-    devolverReserva(nombre, tamaño, entrega)
-
-}
-
-function devolverReserva(nombreReserva, tamañoReserva){
-    let numeroReserva = 2;
-    alert("Gracias, "+ nombreReserva + "! Se reservó un postre " + tamañoReserva + ". Su número de reserva es el numero " + numeroReserva)
-
-}
+alert ("El valor de cada cuota es de $: " + suma(numero1, numero2))
