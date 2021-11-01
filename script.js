48f8b98bc54156095d66ce87b4a8ae7170bd8c87
belenmazza <belenmazza200@gmail.com>
2021-11-01 16:27:29 -0300

    Desafio 6

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
-class Postre1 {
-    constructor (pedido){
-        this.nombre = pedido.nombre
-        this.tamanio = pedido.tamanio
-        this.chocolate = pedido.chocolate
-        this.dulcedeleche = pedido.dulcedeleche
-    }
-    entregarPostre () {
-        if(this.chocolate != "" && this.dulcedeleche != ""){
-            console.log ("Felicitaciones" + " "+ this.nombre + " " + "¡Aquí tiene su postre" + " " + this.tamanio + " " + "con extra" + " " + this.chocolate + 
-            " " +"y extra" + " " + this.dulcedeleche + "!")
-        }else if (this.chocolate != "" && this.dulcedeleche == "") {
-            console.log ("Felicitaciones" + " "+ this.nombre + " " + "¡Aquí tiene su postre" + " " + this.tamanio + " " + "con extra" + " " + this.chocolate + 
-            " " + "!")
-        }else {
-            console.log ("Felicitaciones"+ " " + this.nombre + " " + "Aquí tiene su" + " " + this.postre + " " + this.tamanio)
-        }
-    }
-}
+const postres = ["Rogel", "Lemon Pie", "Brownie", "Torta Cabsha", "Crumble de Manzana"]
+console.log(postres)
 
-let postre = new Postre1 ({nombre: "belen", tamanio:"grande", chocolate:"chocolate", dulcedeleche:"dulce de leche"})
-postre.entregarPostre()
+postres.push ("Tarta de Frutillas")
+
+postres.forEach((value) => {
+    console.log(value);
+})
